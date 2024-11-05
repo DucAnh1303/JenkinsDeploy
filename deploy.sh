e7051548c165dafe62cf2fe494a352ee7fc3f7de
Hung.Ngo (Michael) /Sotatek/DnU <e.hung.ngo@wesang.com>
2024-11-05 22:15:37 +0700

    update

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+git pull origin master
+
+./gradlew clean build -x test
+
+docker stop run_project_jenkins || true
+docker rm run_project_jenkins || true
+
+docker build -t run_project_jenkins .
+
+docker run -d -p 9091:9091 --name application run_project_jenkins
